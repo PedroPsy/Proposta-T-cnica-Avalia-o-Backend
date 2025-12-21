504463f42b7035bfaf8f1d6bf78e3477b164dc3a
PedroPsy <miguelkarate8@gmail.com>
2025-12-21 18:17:28 -0300

    corrigindo a importação das rotas

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from "express";
-import routes from "./routes";
+import routes from "./routes/index";
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.get("/ping", (req, res) => {
+  res.json({ ok: true });
+});
+
 
 export default app;
